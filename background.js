c3fdb60636fb4b8b8223d457c9ba3848fa6c4a73
Joachim Bingel <joa.bingel@gmail.com>
2016-10-19 16:53:01 +0200

    on simple HTML (Gutenberg style): sending node text and offset via POST works

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 /**
  * Created by joachim on 10/13/16.
  */
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript(null, {file: "smplfy.js"});
+chrome.browserAction.onClicked.addListener(function(tabId) {
+    chrome.tabs.executeScript(null, {file: "jquery-3.1.1.js"}, function(){
+        chrome.tabs.executeScript(null, {file: "smplfy.js"});
+    });
 });
--- smplfy.js
+++ smplfy.js
@@ -5,10 +5,61 @@
 function doSomethingWithSelectedText() {
     var selection = getSelectionRange();
     if (selection) {
-        var startNode = selection[0];
-        var endNode = selection[2];
-        // TODO: use ajax to connect to REST, probably gotta import ajax
-        // in background page of extension? [edit: already done]
+        // console.log(selection[0]);
+        // console.log(selection[0].toString());
+        // console.log(JSON.stringify(selection[0]));
+        var startNode = selection[0].parentNode;
+        var endNode = selection[2].parentNode;
+        console.log(startNode);
+        console.log(endNode);
+        var post_data = {};
+        post_data["startNode"] = startNode.textContent;
+        post_data["startOffset"] = selection[1];
+        post_data["endNode"] = endNode.textContent;
+        post_data["endOffset"] = selection[3];
+        post_data["textBetween"] = null;
+        if (startNode != endNode) {
+            var textBetween = "";
+            var curNode = startNode.nextSibling;
+            while (curNode && curNode != endNode) {
+                console.log(curNode);
+                textBetween = textBetween.concat(" ");
+                textBetween = textBetween.concat(curNode.textContent);
+                curNode = curNode.nextSibling;
+                console.log(textBetween);
+            }
+            post_data["textBetween"] = textBetween;
+        }
+
+        //
+        // var post_data = {
+        //     "startNode": selection[0],
+        //     "startOffset": selection[1],
+        //     "endNode": selection[2],
+        //     "endOffset": selection[3]
+        // };
+        console.log(JSON.stringify(post_data));
+        $(document).ready(function() {
+            // $.post("http://localhost:5000/post",
+            //     {"myarg": 3},
+            //     function (response, status) {
+            //         console.log(response);
+            //         alert(response.status);
+            //         alert(response.myarg);
+            //     }
+            // );
+            //
+            $.ajax({
+                type: "POST",
+                contentType: "application/json; charset=utf-8",
+                headers: { 'access-control-allow-origin': '*' },
+                url: "http://127.0.0.1:5000/post",
+                data: JSON.stringify(post_data)
+            }).then(function(data) {
+                $('.myarg').append(data.myarg);
+                console.log(data.myarg);
+            });
+        });
     }
 }
 
@@ -47,7 +98,8 @@ function getSelectionRange() {
         end = sel.anchorOffset;
     }
     if (startNode == endNode && start == end) {
-        alert("Please don't select using doubleclick.");
+        alert("Please don't select using doubleclick. TODO do nothing.");
+    //    TODO do nothing
     }
     console.log([startNode, start, endNode, end]);
     return [startNode, start, endNode, end];
